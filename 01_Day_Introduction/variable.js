039799b0a4c4bccd29d8e11583c2055fbd68d747
es-u <104698204+es-u@users.noreply.github.com>
2023-02-02 10:55:24 +0300

    Update variable.js

--- 01_Day_Introduction/variable.js
+++ 01_Day_Introduction/variable.js
@@ -1,7 +1,7 @@
 // Declaring different variables of different data types
 let firstName = 'Asabeneh' // first name of a person
 let lastName = 'Yetayeh' // last name of a person
-let country = 'Finland' // country
+let country = 'Finland' // country.
 let city = 'Helsinki' // capital city
 let age = 100 // age in years
 let isMarried = true
